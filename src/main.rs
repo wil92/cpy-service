842ce94f3aa60d9d74000023aa6a799492d388ed
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2023-07-26 20:47:27 +0200

    Update dependencies and initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tokio::net::TcpListener;
+
+mod communication;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4646").await?;
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            loop {
+                let n = socket.read(&mut buf).await;
+
+                if n == 0 {
+                    return;
+                }
+
+                socket.write_all(&buf[0..n]).await;
+            }
+        });
+    }
 }
