09e60fc88278559be58026db3331c6b3a5e5f073
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-08-01 10:38:29 -0600

    Integration with the main proccess

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::io;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
-
 mod communication;
+mod protocol;
+
+use std::io;
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::{spawn, time};
+use crate::communication::{connect_to_slave_service, start_slave_service};
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server = true;
 
     if server {
@@ -13,7 +17,7 @@ async fn main() -> io::Result<()> {
         loop {
             let (mut socket, _) = listener.accept().await?;
 
-            tokio::spawn(async move {
+            spawn(async move {
                 let mut buf = vec![0; 1024];
 
                 loop {
@@ -28,7 +32,21 @@ async fn main() -> io::Result<()> {
             });
         }
     } else {
-        // todo
+        // Start both master and slave services on separate tasks.
+        let slave_task = spawn(start_slave_service("0.0.0.0:5000".parse().unwrap()));
+
+        // Wait for a while to make sure the slave service is up and running.
+        //time::delay_for(std::time::Duration::from_secs(1)).await;
+
+        // Connect to the slave service and send a message.
+        if let Err(e) = connect_to_slave_service("127.0.0.1:5000".parse().unwrap(), "Hello, Slave!").await {
+            println!("Error connecting to the slave: {:?}", e);
+        }
+
+        // Wait for the slave task to finish before exiting the program.
+        if let Err(e) = slave_task.await {
+            println!("Error running slave service: {:?}", e);
+        }
     }
     Ok(())
 }
